c1ea6418a397298e25cab908b26c21ce9975e269
Rudi Strydom <iam@thatguy.co.za>
2016-03-23 17:27:38 +0200

    Added working menu
    Using transitions
    
    Added menu controller

--- app/app.js
+++ app/app.js
@@ -1,5 +1,39 @@
 var app = angular.module("app", []);
-    app.controller("indexCtrl", ["$scope", function($scope){
+
+    // Menu Controller
+    app.controller("menuCtrl", ["$scope", "$log", function($scope, $log) {
+        $scope.menuOpen = false;
+        $scope.toggleMenu = function() {
+            $scope.menuOpen = ($scope.menuOpen == false ? true: false);
+        };
+
+        // Used to dismiss all interactions
+        $scope.dismissAll = function($event) {
+            var element = angular.element($event.target);
+
+            // And open Menu
+            if (!element.parent().hasClass('burger-menu') && !element.hasClass('burger-menu__content')) {
+                $scope.menuOpen = false;
+            }
+        };
+
+        $scope.shout = function(message) {
+            alert(message);
+        };
+
+        $scope.menus = [{
+            html: "Home",
+            title: "Go to the home screen",
+            icon: "home",
+        },{
+            html: "About",
+            title: "About this application",
+            icon: "info-sign",
+        }];
+    }]);
+
+    // Index Controller
+    app.controller("indexCtrl", ["$scope", function($scope) {
         $scope.message = "World!";
         $scope.loaded = true;
     }]);
