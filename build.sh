b4e3a68d456986a251182e46c8b5f377098e6950
Rudi Strydom <iam@thatguy.co.za>
2016-03-23 17:54:16 +0200

    Adding any argument build toggle
    Some hidden / visible tweaks

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ docker rm -f sassc
 while inotifywait -r -e modify assets/scss;
 do
     echo `date`" RUNNING: "
-    sassc assets/scss/main.scss assets/css/main.css 2>&1 > /tmp/error
+    # No arguments, just run it as is without compressing it
+    if [ $# -eq 0 ]; then
+        sassc assets/scss/main.scss assets/css/main.css 2>&1 > /tmp/error
+    else
+        sassc assets/scss/main.scss -t compressed assets/css/main.css 2>&1 > /tmp/error
+    fi
+
     rc=$?; if [ $rc != 0 ]; then
         notify-send "SASS BUILD FAILED!"
         cat /tmp/error | ccze -A
