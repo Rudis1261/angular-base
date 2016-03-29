50dbebe07516b156c0ea82c794d8e9a622c08b2d
Rudi Strydom <iam@thatguy.co.za>
2016-03-29 21:01:12 +0200

    Shows as a service

--- app/app.js
+++ app/app.js
@@ -1,32 +1,21 @@
-var app = angular.module("app", ['ngResource']);
+var app = angular.module("app", ['api']);
 
     // Index Controller
-    app.controller("indexCtrl", ["$scope", "$resource", function($scope, $resource) {
+    app.controller("indexCtrl", ["$scope", "shows", function($scope, shows) {
         $scope.search = "";
-        $scope.loaded = false;
         $scope.data = {};
+        $scope.loaded = false;
         $scope.titleLength = 13;
-        $scope.cacheTime = 60;
 
         // Clear an input
         $scope.clear = function(input) {
             $scope[input] = "";
         };
 
-        // Load content from the API
-        var seriesData = JSON.parse(sessionStorage.getItem('series'));
-        var now = Math.round(new Date() / 1000);
-        if (!seriesData || (now - seriesData.timestamp) > $scope.cacheTime) {
-            var apiEndpoint = "https://tvtracker.co.za/api/0.1";
-            //var apiEndpoint = "http://dev.tvtracker.co.za/api/0.1";
-            var seriesApi = $resource(apiEndpoint);
-            var series = seriesApi.get({}, function(){
-                $scope.data = series.data;
-                $scope.loaded = true;
-                sessionStorage.setItem('series', JSON.stringify(series));
-            });
-        } else {
-            $scope.data = seriesData.data;
+        // Load the content from the API
+        shows.get({}, function(series){
+            $scope.data = series.data;
             $scope.loaded = true;
-        }
+            localStorage.setItem('series', JSON.stringify(series));
+        });
     }]);
---
+++ app/components/services/data.js
@@ -0,0 +1,24 @@
+angular.module('api', ['ngResource'])
+.factory('shows', ["$resource", function($resource) {
+
+  var cacheTime = 60;
+  var seriesData = JSON.parse(localStorage.getItem('series')) || false;
+  var now = Math.round(new Date() / 1000);
+
+  // Still within Cache time, return the sudo version using sessionStorage
+  if (seriesData && (now - seriesData.timestamp) < cacheTime) {
+    return {
+      get: function(params, fn){
+        return fn(seriesData);
+      },
+    };
+  };
+
+  // Baring a cached version, return the AJAX Response
+  var apiEndpoint = "https://tvtracker.co.za/api/0.1";
+  //var apiEndpoint = "http://dev.tvtracker.co.za/api/0.1";
+  var api = $resource(apiEndpoint);
+  return {
+    get: api.get,
+  };
+}]);
